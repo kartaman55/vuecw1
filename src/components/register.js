d73e2db5f07a56ba5746905c86c0201f9b67b8cb
kartaman55 <isaacburlowork@gmail.com>
2019-10-31 16:14:52 +0100

    Initial commit

---
+++ src/components/register.js
@@ -0,0 +1,51 @@
+var signupApp = new Vue ({
+    el: '#signup',
+    data: {
+        username: '',
+        email: '',
+        password: '',
+    },
+
+    methods:{
+        onSubmit :function(){
+            var users = '';
+            var newUser = this.username;
+            var newEmail = this.email;
+
+            if (localStorage.getItem('users')) {
+                users = JSON.parse(localStorage.getItem('users'));
+            }
+
+            if (users) {
+                if (users.some(function (user) {
+                    return user.username === newUser
+                }))
+
+                {
+                    alert('Account already exists');
+                }
+
+                if (users) {
+                    if (users.some(function (user) {
+                        return user.email === newEmail
+                    }))
+
+                    {
+                        alert('Account already exists');
+                    }
+
+                    else {
+                        alert('Account created');
+                    }
+                }
+
+                users.push({'username': newUser, 'email': newEmail, 'password': this.password});
+            }
+
+            else {
+                users.push({'username': newUser, 'email': newEmail, 'password': this.password});
+                localStorage.setItem('users', JSON.stringify(users))
+            }
+        }
+    }
+})
